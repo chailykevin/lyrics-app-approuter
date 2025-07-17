37ca39b980dfd3c9de817af8a3941c90a2ceb47d
chailykevin <chailykevin@gmail.com>
2025-07-17 17:16:35 +0700

    feat: add updateAlbum server action for album

--- src/app/actions/album.ts
+++ src/app/actions/album.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { album_artist, album_song, albums } from "@/db/schema";
+import { album_artist, album_song, albums, songs } from "@/db/schema";
 import { db } from "@/index";
 import { eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
@@ -68,6 +68,56 @@ export async function addAlbum(formData: FormData) {
   } else {
     return "Gagal";
   }
+}
+
+export async function editAlbum(formData: FormData, albumId: number) {
+  const rawFormData = {
+    title: formData.get("title"),
+    releaseDate: formData.get("releaseDate"),
+    artists: formData.getAll("artists").map(Number),
+    songs: formData.getAll("songs").map(Number),
+  };
+
+  const validationData = await albumSchema.validate(rawFormData);
+
+  await db.transaction(async (tx) => {
+    await tx
+      .update(albums)
+      .set({
+        title: validationData.title,
+        releaseDate: new Date(validationData.releaseDate),
+      })
+      .where(eq(albums.id, albumId));
+
+    await tx.delete(album_artist).where(eq(album_artist.albumId, albumId));
+
+    const albumArtists = validationData.artists.map((artistId) => ({
+      albumId: albumId,
+      artistId: artistId!,
+    }));
+
+    await tx.insert(album_artist).values(albumArtists);
+
+    await tx.delete(album_song).where(eq(album_song.albumId, albumId));
+
+    const albumSongs = validationData.songs.map((songId) => ({
+      albumId: albumId,
+      songId: songId!,
+    }));
+
+    await tx.insert(album_song).values(albumSongs);
+
+    const updatedAlbumData = await db
+      .select()
+      .from(albums)
+      .where(eq(albums.id, albumId));
+
+    if (updatedAlbumData.length === 1) {
+      return "Sukses";
+    } else {
+      return "Gagal";
+    }
+  });
 }
 
 export async function deleteAlbum(formData: FormData) {
