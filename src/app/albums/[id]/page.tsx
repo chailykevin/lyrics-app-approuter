9e8a43c1b520a6e6bc0867215c31157ebd76a3dd
chailykevin <chailykevin@gmail.com>
2025-07-18 10:52:34 +0700

    feat: add album detail page

---
+++ src/app/albums/[id]/page.tsx
@@ -0,0 +1,139 @@
+import UserSongCard from "@/components/UserSongCard";
+import { albums, artists } from "@/db/schema";
+import { db } from "@/index";
+import { format } from "date-fns";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
+
+type Album = {
+  id: number;
+  title: string;
+  releaseDate: string;
+  artists: Artist[];
+  songs: Song[];
+  createdAt: string;
+  updatedAt: string;
+};
+
+type Artist = {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type Song = {
+  id: number;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  artists: Artist[];
+};
+
+export default async function UserDetailAlbum({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const albumId = Number(id);
+
+  if (isNaN(albumId)) {
+    notFound();
+  }
+
+  const response = await db.query.albums.findFirst({
+    where: eq(albums.id, albumId),
+    with: {
+      album_artist: {
+        with: {
+          artist: true,
+        },
+      },
+      album_song: {
+        with: {
+          song: {
+            with: {
+              artist_song: {
+                with: {
+                  artist: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!response) {
+    notFound();
+  }
+
+  const album = {
+    id: response.id,
+    title: response.title,
+    releaseDate: format(
+      new Date(response.releaseDate.toISOString()),
+      "yyyy-MM-dd"
+    ),
+    createdAt: response.createdAt.toISOString(),
+    updatedAt: response.updatedAt.toISOString(),
+    artists: response.album_artist.map((link) => ({
+      ...link.artist,
+      createdAt: link.artist.createdAt.toISOString(),
+      updatedAt: link.artist.updatedAt.toISOString(),
+    })),
+    songs: response.album_song.map((link) => ({
+      ...link.song,
+      createdAt: link.song.createdAt.toISOString(),
+      updatedAt: link.song.updatedAt.toISOString(),
+      artists: link.song.artist_song.map((link) => ({
+        ...link.artist,
+        createdAt: link.artist.createdAt.toISOString(),
+        updatedAt: link.artist.updatedAt.toISOString(),
+      })),
+    })),
+  };
+
+  return (
+    <main className="flex flex-col justify-center items-center w-full gap-6 py-4 px-120">
+      <div className="flex gap-4 w-full">
+        {/* Image */}
+        <div className="flex grow-0 min-w-60 h-60 border-1 rounded-md"></div>
+        {/* Details */}
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col gap-1">
+            <p className="text-blue-300 font-medium">ALBUM</p>
+            <h2 className="text-4xl font-bold">{album.title}</h2>
+            <p className="text-lg">
+              {album.artists.map((artist) => artist.name).join(", ")}
+            </p>
+          </div>
+          <div className="flex flex-row justify-between">
+            <p>Released: {album.releaseDate}</p>
+            <p>
+              {album.songs.length === 1
+                ? album.songs.length + " track"
+                : album.songs.length + " tracks"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 w-full">
+        <h3>Track List</h3>
+        {album.songs.map((song) => {
+          return (
+            <UserSongCard
+              key={song.id}
+              songId={song.id}
+              songName={song.title}
+              createdAt={song.createdAt}
+              songArtists={song.artists}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
+}
