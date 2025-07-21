5635590c46b0978c9f5790b319645c0382a7c4b1
chailykevin <chailykevin@gmail.com>
2025-07-21 13:24:34 +0700

    feat: add header with UserSearchBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import UserSearchBar from "@/components/UserSearchBar";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex flex-row justify-between items-center bg-blue-300 py-2 px-40 text-black">
+          <Link href="/" className="font-bold text-lg">
+            Lyrics Platform
+          </Link>
+          <UserSearchBar />
+        </header>
         {children}
       </body>
     </html>
